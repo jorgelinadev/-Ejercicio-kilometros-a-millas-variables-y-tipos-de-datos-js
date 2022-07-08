bda31c43bacfb18620d2c7d5f3f9b1cce22d3111
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 09:53:58 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//alert ("Mi archivo está bien linkeado")
+
+const km = prompt ('Ingresa un valor en kilómetros');
+
+const resultado = km *0.621371;
+
+alert ('El valor ingresado en kilómetros: ' + km +', equivale a: '+ resultado + ' millas.');
+
+console.log (alert);
+
